08244cd4ffe5c66a14f4c80641c908ef8a0cebc7
Sakariya Hussein <sakariyahussein2@gmail.com>
2026-05-31 23:12:45 +0100

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 7
+float_type = 7.8
+bool_type = True 
+string_type = "sakariya"
+list_type = [1,3,4]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
