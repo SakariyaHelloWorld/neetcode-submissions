271ffb826bee83a43d714641bb8b7172d707f884
Sakariya Hussein <sakariyahussein2@gmail.com>
2026-05-31 23:18:57 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,11 @@
+variable = 7
+
+print(type(variable))
+variable = 7.8
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "sakariya"
+print(type(variable))
+variable = [9,8,5]
+print(type(variable))
